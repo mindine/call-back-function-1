731d9622b18ecf472ba1aa096e6352168237c07a
mindine <agheutatoo@gmail.com>
2022-07-12 22:47:17 -0500

    my filter done

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -21,24 +21,13 @@ let result3 = multiMap("hi", 5, function(s) {
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
-<<<<<<< HEAD
 let multiMap = function(val, n, cb) {
+
   let newVal = val;
   for (let i = 0; i < n; i++) {
     newVal = cb(newVal);
   }
   return newVal;
-=======
-let multiMap = function(val , count , cb) {
-  let newVal = 0;
-  for ( let iterations = 0 ; iterations < count ; iterations++){
-    newVal = cb(newVal);
-  }
-
-
-  return newVal;
-
->>>>>>> 89fe3a9b911a4146306a5d903d0f545dd247738e
 };
 
 let result1 = multiMap(7, 2, function(n) {
@@ -56,20 +45,7 @@ let result3 = multiMap("hi", 5, function(s) {
 });
 console.log(result3); // hi!!!!!
 
-let result1 = multiMap(7, 2, function(n) {
-  return n * 10;
-});
-console.log(result1); // 700
 
-let result2 = multiMap(7, 3, function(n) {
-    return n * 10;
-});
-console.log(result2); // 7000
-
-let result3 = multiMap("hi", 5, function(s) {
-  return s + "!";
-});
-console.log(result3); // hi!!!!!
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = multiMap;
--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -19,11 +19,26 @@ let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
 console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
-let myFilter = function() {
-
+let myFilter = function(arr, cb) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        if (cb(el)) {
+            newArr.push((el))
+        }
+    }
+    return newArr;
 };
 
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
 
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 
 
 
