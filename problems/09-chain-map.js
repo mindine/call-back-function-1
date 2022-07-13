2eb1da1733c09c1676c028eb56e409d094a5243c
Naudica Grigsby <naudicagrigsby@gmail.com>
2022-07-12 21:42:36 -0700

    09 not done

--- problems/09-chain-map.js
+++ problems/09-chain-map.js
@@ -29,9 +29,36 @@ console.log(chainMap(4, square, half));         // 8
 console.log(chainMap(4, half, square));         // 4
 *******************************************************************************/
 
-let chainMap = function() {
+let chainMap = function(val , ...cb ) {
+
+    let res = 0;
+    for ( let i = 0 ; i < cb.length ; i++ ){
+        let el =cb[i];
+        res = el(val);
+    }
+    // let res = cb(val);
+
+
+
+     return res;
 
 };
+let add5 = function(n) {
+    return n + 5;
+};
+
+let half = function(n) {
+    return n / 2;
+};
+
+let square = function(n) {
+    return n * n;
+};
+console.log(chainMap(25, add5));                // 30
+console.log(chainMap(25, add5, half));          // 15
+console.log(chainMap(25, add5, half, square));  // 225
+console.log(chainMap(4, square, half));         // 8
+console.log(chainMap(4, half, square));         // 4
 
 
 
