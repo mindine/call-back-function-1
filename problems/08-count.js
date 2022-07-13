d8bfe0b2da06e6039b6f88fb9f1ab6b71921a23a
mindine <agheutatoo@gmail.com>
2022-07-12 23:11:16 -0500

    my count done

--- problems/08-count.js
+++ problems/08-count.js
@@ -26,12 +26,37 @@ let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str)
 console.log(result4); // 0
 *******************************************************************************/
 
-let count = function() {
-
+let count = function(arr, cb) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        if (cb(el)) {
+            num ++;
+        }
+    }
+    return num;
 };
 
 
+let result1 = count([18, 5, 32, 7, 100], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1); // 3
 
+let result2 = count([17, 5, 31, 7, 100], function (n) {
+    return n % 2 === 0;
+});
+console.log(result2); // 1
+
+let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+    return str.includes('o');
+});
+console.log(result3); // 3
+
+let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+    return str.includes('x');
+});
+console.log(result4); // 0
 
 
 
