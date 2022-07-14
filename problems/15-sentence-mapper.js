3039b27b29a2cdff2244c0702955bb0af05edca0
mindine <agheutatoo@gmail.com>
2022-07-13 23:29:01 -0500

    sentence mapper done

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -25,10 +25,36 @@ let result2 = sentenceMapper("this is pretty cool right", removeVowels);
 console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
-let sentenceMapper = function() {
+let sentenceMapper = function (sentence, cb) {
+    let words = sentence.split(' ');
+    let newSentence = [];
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        newSentence.push(cb(word));
+    }
+
+    return newSentence.join(' ');
 
 };
 
+let result1 = sentenceMapper("what is the answer?", function (word) {
+    return word.toUpperCase() + "!";
+});
+console.log(result1); // 'WHAT! IS! THE! ANSWER?!'
+
+let removeVowels = function (word) {
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+        let char = word[i];
+        if (!"aeiou".includes(char)) {
+            newWord += char;
+        }
+    }
+    return newWord;
+};
+
+let result2 = sentenceMapper("this is pretty cool right", removeVowels);
+console.log(result2); // 'ths s prtty cl rght'
 
 
 
